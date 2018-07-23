79022959524b8f37eb0fa217e7dbbb5d9c166daf
Richard Tom <tomrichard14@gmail.com>
2018-07-23 14:20:25 -0700

    updated headers

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,46 @@
 import React from 'react';
 
 import './header.css';
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
 
-export default function Header() {
-  return (
-  <header> 
-    <nav> 
-      <ul className="clearfix">
-        <li> 
-          <a className="What?" href="#">What? </a> 
-        </li> 
-        <li> 
-          <a className="+ New Game" href="#">+ New Game </a> 
-        </li>
-      </ul>
-    </nav>
-    <h1>Hot or Cold?</h1> 
-  </header> )
+  }
+  toggleModal() {
+     console.log('hello')
+  }
+  render() {
+
+    return (
+        <header> 
+          <nav> 
+            <ul className="clearfix">
+              <li> 
+                <a className="What?" href="#" onClick={this.toggleModal}>What? </a> 
+              </li> 
+              <li> 
+                <a className="+ New Game" href="#">+ New Game </a> 
+              </li>
+            </ul>
+          </nav>
+          <h1>Hot or Cold?</h1> 
+        </header> )
+  }
 }
+// export default function Header() {
+//   return (
+//   <header> 
+//     <nav> 
+//       <ul className="clearfix">
+//         <li> 
+//           <a className="What?" href="#">What? </a> 
+//         </li> 
+//         <li> 
+//           <a className="+ New Game" href="#">+ New Game </a> 
+//         </li>
+//       </ul>
+//     </nav>
+//     <h1>Hot or Cold?</h1> 
+//   </header> )
+// }
